7a1fa4a7a170926c5b0734aaf2e0c7d95621b931
Sangeetha <sangeetha@Sangeethas-MacBook-Air.local>
2025-06-23 16:51:25 +0530

    Remove .terraform directories and state files from git cache

--- terraform/day3-modules/terraform-modules/ec2-instance/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "web" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  count         = var.instance_count
-
-  tags = {
-    Name = "Module-EC2-${count.index}"
-  }
-}
--- terraform/day3-modules/terraform-modules/ec2-instance/outputs.tf
+++
@@ -1,9 +0,0 @@
-# outputs.tf inside the module
-
-output "public_ips" {
-  value = [for instance in aws_instance.web : instance.public_ip]
-}
-
-output "instance_ids" {
-  value = [for instance in aws_instance.web : instance.id]
-}
